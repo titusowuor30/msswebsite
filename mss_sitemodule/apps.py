99e960958df19d50e995b052314fe6fca7ecc846
unknown <titusowuor30@gmail.com>
2021-11-05 14:44:57 +0300

    initial commit

---
+++ mss_sitemodule/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MssSitemoduleConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mss_sitemodule'
---
+++ mss_website/urls.py
@@ -0,0 +1,32 @@
+"""mss_website URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import url
+from django.views.static import serve
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+from mss_sitemodule.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',home,name='home'),
+    path('services/',services,name='services'),
+    path('contact/',contact,name='contact'),
+    path('about/',about,name='about'),
+    path('more/',more,name='more'),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
