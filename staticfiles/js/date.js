1c25015989f25421afbdbfc1ada664c5624e5bab
titusowuor30 <titusowuor30@gmail.com>
2021-11-30 01:22:54 +0300

    new ui changes

--- staticfiles/js/date.js
+++
@@ -1,6 +0,0 @@
-function diff_years(dt2, dt1) {
-
-    var diff = (dt2.getTime() - dt1.getTime()) / 1000;
-    diff /= (60 * 60 * 24);
-    document.getElementById("mss_at").innerHTML ="MSS @"+Math.abs(Math.round(diff / 365.25));
-}
