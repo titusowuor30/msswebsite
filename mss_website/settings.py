79c54b8597ec2591a119e717951a970291caca95
titusowuor30 <titusowuor30@gmail.com>
2021-12-01 18:42:25 +0300

    new changes

--- mss_website/settings.py
+++ mss_website/settings.py
@@ -81,22 +81,23 @@ WSGI_APPLICATION = 'mss_website.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
+else:        
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'mss_website',
+            'HOST':'localhost',
+            'USER':'root',
+            'PASWWORD':'',
+        }
     }
-}
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mss',
-        'HOST':'localhost',
-        'USER':'root',
-        'PASWWORD':'',
-    }
-}"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
